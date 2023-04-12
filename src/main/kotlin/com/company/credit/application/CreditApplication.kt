b525b3b69f4e8ccf2a5a63c1825dee24e5de9877
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-04-12 19:17:13 -0300

    first commit

---
+++ src/main/kotlin/com/company/credit/application/CreditApplication.kt
@@ -0,0 +1,11 @@
+package com.company.credit.application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplication
+
+fun main() {
+	runApplication<CreditApplication>()
+}

d6739ef5ab0f8a9f93514ca2054bb6e4fbc00b4f
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-04-13 12:28:51 -0300

    Repository Package

---
+++ src/main/kotlin/com/company/credit/application/repository/CostumerRepository.kt
@@ -0,0 +1,9 @@
+package com.company.credit.application.repository
+
+import com.company.credit.application.entity.Costumer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CostumerRepository: JpaRepository<Costumer, Long> {
+}
---
+++ src/main/kotlin/com/company/credit/application/repository/CreditRepository.kt
@@ -0,0 +1,16 @@
+package com.company.credit.application.repository
+
+import com.company.credit.application.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface CreditRepository : JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: UUID): Credit?
+
+    @Query(value = "SELECT * FROM CREDIT WHERE COSTUMER_ID == ?1", nativeQuery = true)
+
+    fun findAllByCostumer(costumerId: Long):List<Credit>
+}

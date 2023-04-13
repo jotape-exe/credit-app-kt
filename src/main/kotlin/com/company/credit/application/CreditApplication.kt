06c9dc4e9d74d461e6d5bdbe1ec1486fe23173dc
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-04-13 12:30:07 -0300

    Enable Feing

--- src/main/kotlin/com/company/credit/application/CreditApplication.kt
+++ src/main/kotlin/com/company/credit/application/CreditApplication.kt
@@ -2,8 +2,10 @@ package com.company.credit.application
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class CreditApplication
 
 fun main() {

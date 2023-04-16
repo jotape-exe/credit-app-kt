634f9fc12d9422bc31d84228a69c72db8bbfa2e6
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-04-16 00:17:01 -0300

    Costumer DTO

--- src/main/kotlin/com/company/credit/application/dto/CostumerDTO.kt
+++ src/main/kotlin/com/company/credit/application/dto/CostumerDTO.kt
@@ -1,12 +1,29 @@
 package com.company.credit.application.dto
 
-data class CostumerDTO (
-    val username:String,
-    val cpf:String,
-    val income:String,
+import com.company.credit.application.entity.Address
+import com.company.credit.application.entity.Costumer
+import java.math.BigDecimal
+
+data class CostumerDTO(
+    val username: String,
+    val cpf: String,
+    val income: BigDecimal,
     val street: String,
     val neighborhood: String,
     val locality: String,
     val uf: String,
     val zipCode: String
-)
+) {
+    fun toEntity(): Costumer = Costumer(
+        username = this.username,
+        cpf = this.cpf,
+        income = this.income,
+        address = Address(
+            street = this.street,
+            neighborhood = this.neighborhood,
+            locality = this.locality,
+            uf = this.uf,
+            zipCode = this.zipCode
+        ),
+    )
+}
